108388fcaca98716e1077ea32587d0fac67c7c16
v-atamanenko <v@atamanenko.in>
2023-01-13 04:31:19 +0400

    Add fake accelerometer and lerp for analogs

--- loader/reimpl/controls.c
+++ loader/reimpl/controls.c
@@ -2,9 +2,96 @@
 #include <psp2/kernel/threadmgr.h>
 #include "main.h"
 #include "controls.h"
+#include "utils/settings.h"
 
 int lastX[SCE_TOUCH_MAX_REPORT] = {-1, -1, -1, -1, -1, -1, -1, -1};
 int lastY[SCE_TOUCH_MAX_REPORT] = {-1, -1, -1, -1, -1, -1, -1, -1};
+
+
+float lerp(float x1, float y1, float x3, float y3, float x2) {
+    return ((x2-x1)*(y3-y1) / (x3-x1)) + y1;
+}
+
+float coord_normalize(float val, float deadzone, float max) {
+    float sign = 1.0f;
+    if (val < 0) sign = -1.0f;
+
+    if (fabsf(val) < deadzone) return 0.f;
+    return lerp(0.f, deadzone * sign, 1.0f*sign, max*sign, val);
+}
+
+typedef struct measurings {
+    float x;
+    float y;
+    float z;
+    uint64_t time;
+} measurings;
+
+measurings fakeAccel[] = {
+        { -0.998903f, -0.244644f, -1.292267f, 75 },
+        { -0.954221f, -0.367070f, -1.234914f, 75 },
+        { -1.029063f, -0.042471f, -1.256281f, 75 },
+        { -0.983264f, -0.268230f, -1.131455f, 75 },
+        { -1.049170f, -0.140187f, -1.296765f },
+        { -1.113959f, -0.304173f, -1.339498f, 75 },
+        { -1.655727f, 2.366763f, -2.261636f, 75 },
+        { 4.224865f, -2.941413f, -0.447724f, 75 },
+        { 2.926493f, -1.684568f, -1.615015f, 75 },
+        { -1.694824f, 1.514265f, -1.591399f, 75 },
+        { -2.287976f, 0.611223f, -1.149448f, 75 },
+        { -1.193269f, -0.221057f, -0.920038f, 75 },
+        { -1.024595f, -0.370441f, -1.369861f, 75 },
+        { -1.054755f, -0.157035f, -1.234914f, 75 },
+        { -1.159758f, -0.240151f, -1.291142f, 75 },
+        { -0.938582f, -0.179499f, -1.225918f, 75 },
+        { -1.046936f, -0.228920f, -1.210174f, 75 },
+        { -1.001137f, -0.234535f, -1.270900f, 75 },
+        { -1.055872f, -0.152542f, -1.367612f, 75 },
+        { -0.945284f, -0.057073f, -1.139327f, 75 },
+        { -1.002254f, -0.198594f, -1.224793f, 75 },
+        { -0.747567f, -0.235659f, -1.256281f, 75 },
+        { -0.699534f, -0.044716f, -1.474445f, 75 },
+        { -0.711821f, 0.117022f, -1.409221f, 75 },
+        { -1.023478f, 0.222602f, -1.628510f, 75 },
+        { -0.848101f, -0.006528f, -2.083956f, 75 },
+        { -0.272822f, -0.489498f, -3.042079f, 75 },
+        { -0.527508f, -0.292941f, -3.173653f, 75 },
+        { -1.081564f, 0.283254f, -3.189397f, 75 },
+        { -1.405508f, 0.348398f, -2.807047f, 75 },
+        { -1.814348f, 0.492166f, -2.401081f, 75 },
+        { -1.502691f, 0.095681f, -2.793552f, 75 },
+        { -1.847859f, 0.522491f, -3.261368f, 75 },
+
+        // for zero g jump
+        { -1.761846f, -0.322144f, -1.657748f, 250 },
+        { -1.188801f, -0.022254f, -1.695983f, 75 },
+        { -0.794483f, -0.263738f, -2.931873f, 75 },
+        { -0.745333f, -0.150296f, -2.825040f, 75 },
+        { -0.873793f, -0.444571f, -1.743215f, 75 },
+        { -0.804536f, -0.127832f, -3.795533f, 75 },
+        { -1.742857f, 0.038399f, -1.233790f, 75 },
+        { -0.812356f, 0.224848f, -2.738449f, 75 },
+        { 0.863030f, 3.779731f, -9.721955f, 75 },
+        { -4.183607f, 3.622485f, 8.522037f, 75 },
+        { -6.169717f, 7.627765f, 23.052782f, 75 },
+        { -0.738630f, 6.358565f, 14.906132f, 75 },
+        { 0.151609f, 2.630712f, 9.449697f, 75 },
+        { 0.831056f, 3.416942f, 6.462183f, 75 },
+        { -2.905704f, 5.808204f, -9.787180f, 75 },
+        { -2.807403f, 1.275026f, -4.152019f, 75 },
+        { -2.541546f, -0.363701f, 0.349680f, 75 },
+        { -2.520322f, 0.236080f, -2.166049f, 75 },
+        { -1.934989f, -0.196346f, -0.763724f, 75 },
+        { -2.559419f, -0.453556f, 0.108265f, 75 },
+        { -1.904829f, -0.004282f, -1.038117f, 75 },
+        { -2.014299f, -0.291818f, -0.051879f, 75 },
+        { -2.021002f, -0.269354f, -1.045988f, 75 },
+        { -1.804294f, -0.230042f, -1.018999f, 75 },
+        { -2.016533f, -0.192978f, -0.518571f, 75 },
+        { -1.768549f, -0.101999f, -1.323754f, 75 },
+        { -1.541788f, -0.406383f, -1.025746f, 75 },
+};
+
 
 void (*NativeOnPointerEvent)(JNIEnv *env, jobject obj, int rawEvent, int moduleId, int eventPointerId, float eventX, float eventY);
 void (*NativeOnKeyDown)(JNIEnv *env, jobject obj, int moduleId, int androidKey, int altPressed);
@@ -28,6 +115,20 @@ void controls_poll() {
     pollTouch();
     pollPad();
     pollAccel();
+}
+
+volatile int fakeAccelRunning = 0;
+
+int fakeAccelThread(SceSize args, void* argp) {
+    int count = sizeof(fakeAccel) / sizeof(measurings);
+    printf("sizeof fakeAccel %i\n", count);
+    for (int i = 0; i < count; ++i) {
+        sceKernelDelayThread(fakeAccel[i].time * 100);
+        NativeOnAcceleration(&jni, (void*)0x42424242, fakeAccel[i].x, fakeAccel[i].y, fakeAccel[i].z);
+    }
+    fakeAccelRunning = 0;
+    printf("1=>0\n");
+    return sceKernelExitDeleteThread(0);
 }
 
 void pollTouch() {
@@ -103,44 +204,37 @@ int pollPad() {
         for (int i = 0; i < sizeof(mapping) / sizeof(ButtonMapping); i++) {
             if (pressed_buttons & mapping[i].sce_button) {
                 //debugPrintf("NativeOnKeyDown %i\n", mapping[i].android_button);
-                NativeOnKeyDown(&jni, (void *) 0x42424242, 600, mapping[i].android_button, 1);
+                if (mapping[i].sce_button == SCE_CTRL_UP && fakeAccel_enabled) {
+                    if (fakeAccelRunning == 0) {
+                        fakeAccelRunning = 1;
+                        SceUID fake_accel_thread = sceKernelCreateThread("fakeaccel", fakeAccelThread, 0x40, 0x1000, 0, 0, NULL);
+                        sceKernelStartThread(fake_accel_thread, 0, NULL);
+                    }
+                } else {
+                    NativeOnKeyDown(&jni, (void *) 0x42424242, 600, mapping[i].android_button, 1);
+                }
+
             }
             if (released_buttons & mapping[i].sce_button) {
                 //debugPrintf("NativeOnKeyUp %i\n", mapping[i].android_button);
-                NativeOnKeyUp(&jni, (void *) 0x42424242, 600, mapping[i].android_button, 1);
+                if (mapping[i].sce_button != SCE_CTRL_UP || !fakeAccel_enabled) {
+                    NativeOnKeyUp(&jni, (void *) 0x42424242, 600, mapping[i].android_button, 1);
+                }
+
             }
         }
     }
 
     // Analog sticks
-    // Here we are trying to use real analog sticks to emulate Xperia Play's virtual analog sticks
-    // that are trying to emulate real analog sticks :)
-
-    // If you read Xperia Play's developers manual, its touchpad is basically a second touch screen sized 966x360.
-    // I assumed that a 360x360 on each side would be the working surface for our "analog sticks" and mapped
-    // the values accordingly. There is one caveat though: the camera movement is rather gesture-based, so we can't just
-    // keep sending PointerMove events when stick is pushed completely to one side. That's why you can see a hack below
-    // where `if (fabsf(rx) > 0.88f || fabsf(ry) > 0.38f)` we send touch up, touch down at (virtual) 0;0,
-    // and only then move.
-
-    lx = ((float)pad.lx - 128.0f) / 128.0f;
-    ly = ((float)pad.ly - 128.0f) / 128.0f;
-    rx = ((float)pad.rx - 128.0f) / 128.0f;
-    ry = ((float)pad.ry - 128.0f) / 128.0f;
-
-    if (fabsf(lx) < 0.10f)
-        lx = 0.0f;
-    if (fabsf(ly) < 0.10f)
-        ly = 0.0f;
-    if (fabsf(rx) < 0.10f)
-        rx = 0.0f;
-    if (fabsf(ry) < 0.10f)
-        ry = 0.0f;
+    lx = coord_normalize(((float)pad.lx - 128.0f) / 128.0f, leftStickDeadZone, 0.76f);
+    ly = coord_normalize(((float)pad.ly - 128.0f) / 128.0f, leftStickDeadZone, 0.76f);
+    rx = coord_normalize(((float)pad.rx - 128.0f) / 128.0f, rightStickDeadZone, 1.0f);
+    ry = coord_normalize(((float)pad.ry - 128.0f) / 128.0f, rightStickDeadZone, 1.0f);
 
     float touchLx_radius = 75;
     float touchLy_radius = 75;
-    float touchRx_radius = 150;
-    float touchRy_radius = 100;
+    float touchRx_radius = 155;
+    float touchRy_radius = 105;
 
     float touchLx_base = 192;
     float touchLy_base = 75;
@@ -187,11 +281,10 @@ int pollPad() {
     lastRx = rx;
     lastRy = ry;
 
-    touchRx_last = touchRx_base + touchRx_radius * lastRx;
-    touchRy_last = touchRy_base + touchRy_radius * (lastRy * (-1));
-
-    touchLx_last = touchLx_base + touchLx_radius * lastLx;
-    touchLy_last = touchLy_base + touchLy_radius * (lastLy);
+    touchRx_last = touchRx;
+    touchRy_last = touchRy;
+    touchLx_last = touchLx;
+    touchLy_last = touchLy;
 
     return 1;
 }
@@ -204,6 +297,8 @@ float accel_last_z;
 #define SHAKE_THRESHOLD 300
 
 void pollAccel() {
+    if (fakeAccel_enabled) return;
+
     uint64_t curTime = currenttime_ms();
 
     if (lastUpdate == 0) {
